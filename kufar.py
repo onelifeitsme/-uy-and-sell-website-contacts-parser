a41f3495579f1ee303ea5245e1fd3c5d5eeb4be6
Artur Karpovich <onelifeitsme@gmail.com>
2022-02-01 17:08:27 +0300

    corrected_by_flake8

--- kufar.py
+++ kufar.py
@@ -6,8 +6,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 
-HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36', 'accept': '*/*', 'x-client-data': 'VGo9iQEIorbJAQjEtskBCKmdygEI9tDKAQiMnssBCKKgywEI3PLLAQjv8ssBCM72ywEIs/jLAQie+csBCPv5ywEIvv8KBU=='}
+
+HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
+                         '(KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36',
+           'accept': '*/*', 'x-client-data':
+               'VGo9iQEIorbJAQjEtskBCKmdygEI9tDKAQiMnssBCKKgywEI3PLLAQjv8ssBCM72ywEIs/jLAQie+csBCPv5ywEIvv8KBU=='}
 
 print("Введите запрос для парсинга на Куфаре")
 product = input()
@@ -18,9 +23,6 @@ driver = webdriver.Chrome()
 # ЗАХОДИМ НА САЙТ С ЗАПРОСОМ В ПОИСКЕ, ВКЛЮЧАЮЩЕМ ВВЕДЁННЫЙ ТОВАР
 driver.get(f'https://www.kufar.by/listings?ot=1&query={product}')
 sleep(1)
-# if driver.find_element_by_xpath('//*[@id="portal"]/div/div[2]/div[1]/div'):
-#     driver.find_element_by_xpath('//*[@id="portal"]/div/div[2]/div[1]/div/div[3]/div/img').click()
-# sleep(2)
 
 
 # АВТОРИЗАЦИЯ
@@ -44,8 +46,9 @@ items = []
 pages_number = driver.find_element(By.XPATH, '//div[@data-name="listings-pagination"]').text
 pages_number = int(pages_number[-1])
 
-# ПОЛУЧЕНИЕ ССЫЛОК НА ОБЪЯВЛЕНИЯ НА ОДНОЙ СТРАНИЦЕ.ИСКЛЮЧАЕМ ПЛАТНЫЕ ОБЪЯВЛЕНИЯ НЕ ПО ТЕМЕ
+
 def get_items_on_page():
+    """ПОЛУЧЕНИЕ ССЫЛОК НА ОБЪЯВЛЕНИЯ НА ОДНОЙ СТРАНИЦЕ.ИСКЛЮЧАЕМ ПЛАТНЫЕ ОБЪЯВЛЕНИЯ НЕ ПО ТЕМЕ"""
     all_ads = driver.find_elements(By.XPATH, '//article/a')
     minus = driver.find_elements(By.XPATH, '//div[@data-name="card-icon-polepos"]/ancestor::article/a')
     for ad in all_ads:
@@ -53,20 +56,15 @@ def get_items_on_page():
             items.append(ad.get_attribute('href'))
 
 
-
-
-
-
 def next_page(i):
+    """Переход на новую страницу"""
     page = driver.find_element(By.XPATH, f'//div[@data-name="listings-pagination"]/descendant::a[text()="{i}"]')
     page.click()
     sleep(4)
 
 
-#
-#
-# ПРОХОД ВСЕХ СТРАНИЦ ВЫДАЧИ И ПОЛУЧЕНИЕ ССЫЛОК НА ОБЪЯВЛЕНИЯ
 def get_ads_on_all_pages():
+    """ПРОХОД ВСЕХ СТРАНИЦ ВЫДАЧИ И ПОЛУЧЕНИЕ ССЫЛОК НА ОБЪЯВЛЕНИЯ"""
     for i in range(pages_number+1):
         if i > 1:
             next_page(i)
@@ -74,16 +72,11 @@ def get_ads_on_all_pages():
             print(len(items))
 
 
-# sex = items[0]
-# driver.get(sex)
-# sleep(3)
-# driver.find_element(By.XPATH, '//button[@data-name="call_button"]').click()
-#
 def data_parse():
     sleep(3)
     try:
         driver.find_element(By.XPATH, '//button[@data-name="call_button"]').click()
-    except:
+    except NoSuchElementException:
         pass
     sleep(3)
     try:
@@ -93,11 +86,12 @@ def data_parse():
             "name": name,
             "phone": phone,
         })
-    except:
+    except NoSuchElementException:
         pass
 
 
 def writing_data_to_csv():
+    """Запись итоговых данных в файл csv"""
     with open("kufar.csv", "a") as temp:
         polya = ['name', 'phone']
         out = csv.DictWriter(temp, fieldnames=polya)
@@ -121,4 +115,3 @@ for item in items:
     data_parse()
     writing_data_to_csv()
     sleep(4)
-
